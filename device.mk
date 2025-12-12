63935054c0930b7734ce218a2d805ad0e38a7a49
dodyirawan85 <dev.irawans@gmail.com>
2025-12-13 01:02:37 +0700

    sheng: Build XiaomiPen
    Change-Id: I2485ada7a69b31001446c6e775f378ad141d4d27
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
     SystemUIOverlaySheng \
     WifiResSheng
 
+# Pen
+PRODUCT_PACKAGES += \
+    XiaomiPen
+
 # Permission
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
